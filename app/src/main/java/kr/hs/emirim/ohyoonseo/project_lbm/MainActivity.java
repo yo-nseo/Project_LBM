96e2bf091829edf46a9d07c2045ab8e1a17749fa
iruy <jeon1310>
2021-07-28 18:22:57 +0900

    Edit activity_sub2.xml 1차수정

--- app/src/main/java/kr/hs/emirim/ohyoonseo/project_lbm/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ohyoonseo/project_lbm/MainActivity.java
@@ -3,6 +3,9 @@ package kr.hs.emirim.ohyoonseo.project_lbm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
--- app/src/main/java/kr/hs/emirim/ohyoonseo/project_lbm/SubActivity2.java
+++ app/src/main/java/kr/hs/emirim/ohyoonseo/project_lbm/SubActivity2.java
@@ -3,12 +3,32 @@ package kr.hs.emirim.ohyoonseo.project_lbm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.NumberPicker;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class SubActivity2 extends AppCompatActivity {
+    GridView gridL;
+    GridAdapter adapter;
+
+    ArrayList<testDate> td;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub2);
+        gridL = findViewById(R.id.grid_letter);
+
+        td = new ArrayList<>();
+        td.add(new testDate("2021.07.31"));
+        td.add(new testDate("2021.08.10"));
+        td.add(new testDate("2021.08.21"));
+
+        adapter = new GridAdapter(this,td);
+        gridL.setAdapter(adapter);
+
     }
 }
