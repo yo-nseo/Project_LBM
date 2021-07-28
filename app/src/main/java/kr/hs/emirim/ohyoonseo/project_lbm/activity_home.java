4cf102d5335ac185d382dc09ff909387e6717ab6
chaehyeon7 <81363193+chaehyeon7@users.noreply.github.com>
2021-07-28 16:37:29 +0900

    Edit activity_main.xml Commit

---
+++ app/src/main/java/kr/hs/emirim/ohyoonseo/project_lbm/activity_home.java
@@ -0,0 +1,13 @@
+package kr.hs.emirim.ohyoonseo.project_lbm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
