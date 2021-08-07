1950e59678f93387096d6e4336f36495312361ed
iruy <jeon1310>
2021-08-08 02:09:55 +0900

    Edit SubActivity2 longclick 시 편지 삭제 처리

--- app/src/main/java/kr/hs/emirim/ohyoonseo/project_lbm/GridAdapter.java
+++ app/src/main/java/kr/hs/emirim/ohyoonseo/project_lbm/GridAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -47,4 +48,6 @@ public class GridAdapter extends BaseAdapter {
         txtview.setText(td.get(position).date);
         return convertView;
     }
+
+
 }
--- app/src/main/java/kr/hs/emirim/ohyoonseo/project_lbm/SubActivity2.java
+++ app/src/main/java/kr/hs/emirim/ohyoonseo/project_lbm/SubActivity2.java
@@ -3,21 +3,80 @@ package kr.hs.emirim.ohyoonseo.project_lbm;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SubActivity2 extends AppCompatActivity {
     ImageButton menu;
+    GridView gridL;
+    GridAdapter adapter;
+
+    ArrayList<testDate> td;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub2);
         menu = findViewById(R.id.dot3);
         registerForContextMenu(menu);
+
+        // 데이터 가져옴
+        gridL = findViewById(R.id.grid_letter);
+
+        td = new ArrayList<>();
+        td.add(new testDate("2021.07.31"));
+        td.add(new testDate("2021.08.10"));
+        td.add(new testDate("2021.08.21"));
+        td.add(new testDate("2021.07.31"));
+        td.add(new testDate("2021.08.10"));
+        td.add(new testDate("2021.08.21"));
+        td.add(new testDate("2021.07.31"));
+        td.add(new testDate("2021.08.10"));
+        td.add(new testDate("2021.08.21"));
+        td.add(new testDate("2021.08.21"));
+        td.add(new testDate("2021.07.31"));
+        td.add(new testDate("2021.08.10"));
+        td.add(new testDate("2021.08.21"));
+        td.add(new testDate("2021.08.21"));
+        td.add(new testDate("2021.08.21"));
+        td.add(new testDate("2021.07.31"));
+        td.add(new testDate("2021.08.10"));
+        td.add(new testDate("2021.08.21"));
+
+        adapter = new GridAdapter(this,td);
+        gridL.setAdapter(adapter);
+
+        gridL.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog dir = new AlertDialog.Builder(SubActivity2.this)
+                        .setTitle("편지삭제")
+                        .setMessage(td.get(position)+"편지를 정말 삭제하시겠습니까?")
+                        .setPositiveButton("삭제", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                td.remove(position);
+                                adapter.notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton("취소",null)
+                        .show();
+                return false;
+            }
+        });
     }
 
     @Override
@@ -35,6 +94,7 @@ public class SubActivity2 extends AppCompatActivity {
             case R.id.item_setting:
                 return true;
             case R.id.item_del:
+                Toast.makeText(SubActivity2.this,"편지를 길게 누르면 삭제됩니다",Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.item_sort:
                 return true;
@@ -46,4 +106,5 @@ public class SubActivity2 extends AppCompatActivity {
                 return false;
         }
     }
+
 }
